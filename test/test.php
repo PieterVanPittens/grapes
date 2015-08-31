59fb1d7890ddf9248c8a2e4203beb956e82c6c02
PieterVanPittens <pieter.van.pittens@outlook.com>
2015-08-31 19:23:24 +0200

    added unittests

---
+++ test/test.php
@@ -0,0 +1,33 @@
+<?php
+class MoneyTest extends PHPUnit_Framework_TestCase {
+
+  public function testObjectCanBeConstructedFromStringValue()
+    {
+        $this->assertEquals(true, true);
+    }
+	
+  public function testCanConnectMySql()
+    {
+	$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+$isConnected = false;
+} else {
+$isConnected = true;
+}
+
+
+	$this->assertEquals(true, $isConnected);
+    }
+	
+	
+	
+	
+}
+?>
